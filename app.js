70235c24a63c611bcaea82f38993aeb52b523547
TimothyY <insanelysaneme@live.com>
2014-04-07 02:39:32 +0700

    Add Facebook login 1st attempt

--- app.js
+++ app.js
@@ -8,6 +8,25 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
+var passport = require('passport')
+  , FacebookStrategy = require('passport-facebook').Strategy;
+
+////////////////////////////////////////////////////////////////////////////////////////////
+passport.use(new FacebookStrategy({
+    clientID: "1464692647095538",
+    clientSecret: "3d2b728362149f6e4b1eaa5a0751891d",
+    //callbackURL: "http://www.example.com/auth/facebook/callback"
+    callbackURL: "http://insanelysaneme.azurewebsites.net/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate(..., function(err, user) {
+      if (err) { return done(err); }
+      done(null, user);
+    });
+  }
+));
+///////////////////////////////////////////////////////////////////////////////////////////
+
 var app = express();
 
 GLOBAL.makanapaDB={
@@ -41,6 +60,21 @@ app.post("/makanapabackend/addrestaurant",routes.addrestaurant);
 
 app.post("/getrestaurant",routes.getrestaurant);
 
+///////////////////////////////////////////////////////////////////////////////////////////
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback', 
+  passport.authenticate('facebook', { successRedirect: '/makanapa',
+                                      failureRedirect: '/' }));
+                                      
+///////////////////////////////////////////////////////////////////////////////////////////
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
